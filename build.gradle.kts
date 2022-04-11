102df8f877d71e0eb39a20b6cfae9dddf64a33ee
jprogramer <joseph.debbah.local@gmail.com>
2022-04-11 16:56:57 +0100

    update android gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,10 +13,10 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("com.android.tools.build:gradle:7.1.3")
         classpath("com.google.gms:google-services:4.3.10")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.8.1")
-        classpath("com.google.firebase:perf-plugin:1.4.0")
+        classpath("com.google.firebase:perf-plugin:1.4.1")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
         classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.0")
         // extra dependencies to resolve potential version conflicts
